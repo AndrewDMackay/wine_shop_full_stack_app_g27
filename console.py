def066dfd40cc4b13ee8b1733261534119ddf3e0
Andrew Mackay <andrewmackayd@gmail.com>
2021-09-26 20:01:04 +0100

    sixteenth commit main

--- console.py
+++ console.py
@@ -8,49 +8,49 @@ import repositories.wine_repository as wine_repository
 import repositories.producer_repository as producer_repository
 
 
-# wine_repository.delete_all()
-# producer_repository.delete_all()
+wine_repository.delete_all()
+producer_repository.delete_all()
 
-# producer1 = producer("producer_name1", "country1", "region1", "producer_description1")
-# producer_repository.save(producer1)
+producer1 = producer("producer_name1", "country1", "region1", "producer_description1")
+producer_repository.save(producer1)
 
-# producer2 = producer("producer_name2", "country2", "region2", "producer_description2")
-# producer_repository.save(producer2)
+producer2 = producer("producer_name2", "country2", "region2", "producer_description2")
+producer_repository.save(producer2)
 
-# producer3 = producer("producer_name3", "country3", "region3", "producer_description3")
-# producer_repository.save(producer3)
+producer3 = producer("producer_name3", "country3", "region3", "producer_description3")
+producer_repository.save(producer3)
 
-# producer_repository.select_all()
+producer_repository.select_all()
 
 
-# wine1 = wine("wine_name1", producer1, 6, 5.00, 10.00)
-# wine_repository.save(wine1)
+wine1 = wine("wine_name1", producer1, 6, 5.00, 10.00)
+wine_repository.save(wine1)
 
-# wine2 = wine("wine_name2", producer1, 6, 5.00, 10.00)
-# wine_repository.save(wine2)
+wine2 = wine("wine_name2", producer1, 6, 5.00, 10.00)
+wine_repository.save(wine2)
 
-# wine3 = wine("wine_name3", producer1, 6, 5.00, 10.00)
-# wine_repository.save(wine3)
+wine3 = wine("wine_name3", producer1, 6, 5.00, 10.00)
+wine_repository.save(wine3)
 
-# wine4 = wine("wine_name4", producer1, 6, 5.00, 10.00)
-# wine_repository.save(wine4)
+wine4 = wine("wine_name4", producer1, 6, 5.00, 10.00)
+wine_repository.save(wine4)
 
-# wine5 = wine("wine_name5", producer1, 6, 5.00, 10.00)
-# wine_repository.save(wine5)
+wine5 = wine("wine_name5", producer1, 6, 5.00, 10.00)
+wine_repository.save(wine5)
 
-# wine6 = wine("wine_name6", producer1, 6, 5.00, 10.00)
-# wine_repository.save(wine6)
+wine6 = wine("wine_name6", producer1, 6, 5.00, 10.00)
+wine_repository.save(wine6)
 
-# wine7 = wine("wine_name7", producer1, 6, 5.00, 10.00)
-# wine_repository.save(wine7)
+wine7 = wine("wine_name7", producer1, 6, 5.00, 10.00)
+wine_repository.save(wine7)
 
-# wine8 = wine("wine_name8", producer1, 6, 5.00, 10.00)
-# wine_repository.save(wine8)
+wine8 = wine("wine_name8", producer1, 6, 5.00, 10.00)
+wine_repository.save(wine8)
 
-# wine9 = wine("wine_name9", producer1, 6, 5.00, 10.00)
-# wine_repository.save(wine9)
+wine9 = wine("wine_name9", producer1, 6, 5.00, 10.00)
+wine_repository.save(wine9)
 
-# wine_repository.select_all()
+wine_repository.select_all()
 
 
 pdb.set_trace()

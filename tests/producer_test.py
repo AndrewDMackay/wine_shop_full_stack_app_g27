ce93a8499894355bfd7e065105d05fbad2c1cd78
Andrew Mackay <andrewmackayd@gmail.com>
2021-09-26 19:49:17 +0100

    fourteenth commit main

--- tests/producer_test.py
+++ tests/producer_test.py
@@ -7,27 +7,23 @@ from models.wine import Wine
 class TestProducer(unittest.TestCase):
     
     def setUp(self):
-        self.producer1 = Producer("producer1", "country1", "region1", "producer_description1")
-        self.wine1 = Wine("wine1", self.producer1, 6, 5.00, 10.00)
+        self.producer = Producer("producer1", "country1", "region1", "producer_description1")
+        self.wine = Wine("wine1", self.producer, 6, 5.00, 10.00)
         
     
     def test_producer_has_producer_name(self):
         self.assertEqual("producer1", self.producer.producer_name)
         
         
-    def test_producer_has_producer(self):
-        self.assertEqual("producer1", self.producer.producer)
+    def test_producer_has_country(self):
+        self.assertEqual("country1", self.producer.country)
        
         
-    def test_producer_has_stock(self):
-        self.assertEqual(6, self.producer.stock)
-
-    def test_producer_has_net_price(self):
-        self.assertEqual(5.00, self.producer.net_price)
-
-    def test_producer_has_sell_price(self):
-        self.assertEqual(10.00, self.producer.sell_price)
+    def test_producer_has_region(self):
+        self.assertEqual("region1", self.producer.region)
 
 
-    
+    def test_producer_has_producer_description(self):
+        self.assertEqual("producer_description1", self.producer.producer_description)
+
 
--- tests/wine_test.py
+++ tests/wine_test.py
@@ -7,8 +7,8 @@ from models.producer import Producer
 class TestWine(unittest.TestCase):
     
     def setUp(self):
-        self.producer1 = Producer("producer1", "country1", "region1", "producer_description1")
-        self.wine1 = Wine("wine1", self.producer1, 6, 5.00, 10.00)
+        self.producer = Producer("producer1", "country1", "region1", "producer_description1")
+        self.wine = Wine("wine1", "producer1", 6, 5.00, 10.00)
         
     
     def test_wine_has_wine_name(self):
@@ -22,12 +22,13 @@ class TestWine(unittest.TestCase):
     def test_wine_has_stock(self):
         self.assertEqual(6, self.wine.stock)
 
+
     def test_wine_has_net_price(self):
         self.assertEqual(5.00, self.wine.net_price)
+
 
     def test_wine_has_sell_price(self):
         self.assertEqual(10.00, self.wine.sell_price)
 
 
     
-

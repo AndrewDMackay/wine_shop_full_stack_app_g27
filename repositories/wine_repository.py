920a74ce6c8ea7ffd6a15fc2439f82b15fd5e1b4
Andrew Mackay <andrewmackayd@gmail.com>
2021-09-26 17:57:00 +0100

    eighth commit main

--- repositories/wine_repository.py
+++ repositories/wine_repository.py
@@ -5,8 +5,8 @@ import repositories.producer_repository as producer_repository
 
 
 def save(wine):
-    sql = "INSERT INTO wines (wine_name, producer_id) VALUES (%s, %s) RETURNING *"
-    values = [wine.wine_name, wine.producer.id]
+    sql = "INSERT INTO wines (wine_name, producer_id, stock, net_price, sell_price) VALUES (%s, %s, %s, %s, %s) RETURNING *"
+    values = [wine.wine_name, wine.producer.id, wine.stock, wine.net_price, wine.sell_price]
     results = run_sql(sql, values)
     id = results[0]["id"]
     wine.id = id
@@ -31,9 +31,11 @@ def select_all():
 
     for row in results:
         producer = producer_repository.select(row['producer_id'])
-        wine = wine(row['wine_name'], producer, row['id'])
+        wine = wine(row['wine_name'], producer, row['stock'], row['net_price'], row['sell_price'], row['id'])
         wines.append(wine)
     return wines
+
+    (wine_name, producer_id, stock, net_price, sell_price)
 
 
 def select(id):
@@ -44,13 +46,13 @@ def select(id):
 
     if result is not None:
         producer = producer_repository.select(result['producer_id'])
-        wine = wine(result['wine_name'], producer, result['id'])
+        wine = wine(result['wine_name'], producer, result['stock'], result['net_price'], result['sell_price'], result['id'])
     return wine
 
 
 def update(wine):
-    sql = "UPDATE wines SET (wine_name, producer, producer_id) = (%s, %s, %s) WHERE id = %s"
-    values = [wine.wine_name, wine.producer.id, wine.id]
+    sql = "UPDATE wines SET (wine_name, producer, producer_id) = (%s, %s, %s, %s, %s) WHERE id = %s"
+    values = [wine.wine_name, wine.producer.id, wine.stock, wine.net_price, wine.sell_price]
     run_sql(sql, values)
 
 

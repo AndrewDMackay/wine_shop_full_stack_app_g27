b959bff1d3170d6e4619528c4b41bd2a17c95ac1
Andrew Mackay <andrewmackayd@gmail.com>
2021-09-26 19:42:23 +0100

    twelth commit main

--- console.py
+++ console.py
@@ -9,43 +9,43 @@ import repositories.producer_repository as producer_repository
 # wine_repository.delete_all()
 # producer_repository.delete_all()
 
-# producer1 = producer(?)
+# producer1 = producer("producer_name1", "country1", "region1", "producer_description1")
 # producer_repository.save(producer1)
 
-# producer2 = producer(?)
+# producer2 = producer("producer_name2", "country2", "region2", "producer_description2")
 # producer_repository.save(producer2)
 
-# producer3 = producer(?)
+# producer3 = producer("producer_name3", "country3", "region3", "producer_description3")
 # producer_repository.save(producer3)
 
 # producer_repository.select_all()
 
 
-# wine1 = wine(?, producer1)
+# wine1 = wine("wine_name1", producer1, 6, 5.00, 20.00)
 # wine_repository.save(wine1)
 
-# wine2 = wine(?, producer1)
+# wine2 = wine("wine_name2", producer1, 6, 5.00, 20.00)
 # wine_repository.save(wine2)
 
-# wine3 = wine(?, producer1)
+# wine3 = wine("wine_name3", producer1, 6, 5.00, 20.00)
 # wine_repository.save(wine3)
 
-# wine4 = wine(?, producer2)
+# wine4 = wine("wine_name4", producer1, 6, 5.00, 20.00)
 # wine_repository.save(wine4)
 
-# wine5 = wine(?, producer2)
+# wine5 = wine("wine_name5", producer1, 6, 5.00, 20.00)
 # wine_repository.save(wine5)
 
-# wine6 = wine(?, producer2)
+# wine6 = wine("wine_name6", producer1, 6, 5.00, 20.00)
 # wine_repository.save(wine6)
 
-# wine7 = wine(?, producer3)
+# wine7 = wine("wine_name7", producer1, 6, 5.00, 20.00)
 # wine_repository.save(wine7)
 
-# wine8 = wine(?, producer3)
+# wine8 = wine("wine_name8", producer1, 6, 5.00, 20.00)
 # wine_repository.save(wine8)
 
-# wine9 = wine(?, producer3)
+# wine9 = wine("wine_name9", producer1, 6, 5.00, 20.00)
 # wine_repository.save(wine9)
 
 # wine_repository.select_all()
---
+++ tests/wine_test.py
@@ -0,0 +1,31 @@
+import unittest
+from models.wine import Wine
+from models.producer import Producer
+
+class TestWine(unittest.TestCase):
+    
+    def setUp(self):
+        self.producer1 = Producer("producer1", "country1", "region1", "producer_description1")
+        self.wine1 = Wine("wine1", self.producer1, 6, 5.00, 10.00)
+        
+    
+    def test_wine_has_wine_name(self):
+        self.assertEqual("wine1", self.wine.wine_name)
+        
+        
+    def test_wine_has_producer(self):
+        self.assertEqual("producer1", self.wine.producer)
+       
+        
+    def test_wine_has_stock(self):
+        self.assertEqual(6, self.wine.stock)
+
+    def test_wine_has_net_price(self):
+        self.assertEqual(5.00, self.wine.net_price)
+
+    def test_wine_has_sell_price(self):
+        self.assertEqual(10.00, self.wine.sell_price)
+
+
+    
+

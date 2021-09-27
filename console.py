dd3ee96300acd180004c9f56997cda5bdb44bd2e
Andrew Mackay <andrewmackayd@gmail.com>
2021-09-27 20:03:16 +0100

    thirty-first commit main, updated all placeholder information

--- console.py
+++ console.py
@@ -14,66 +14,40 @@ producer_repository.delete_all()
 producer1 = Producer("Radikon", "Italy", "Friuli", "Saša Radikon.")
 producer_repository.save(producer1)
 
-producer2 = Producer("Pheasant's Tears", "Georgia", "Kiziqi", "Gela Patalashvili")
+producer2 = Producer("Pheasant's Tears", "Georgia", "Kakheti", "Gela Patalashvili")
 producer_repository.save(producer2)
 
-producer3 = Producer("Sepp Muster", "Austria", "Susteiermark", "Sepp Muster")
+producer3 = Producer("Sepp Muster", "Austria", "Susteiermark", "Sepp, and Maria Muster")
 producer_repository.save(producer3)
 
 producer_repository.select_all()
 
-# class Producer: properties, and priorities..
 
-# Priority	        Property	            Example Input
-# 1	                Producer Name	        Radikon
-# 1	                Country	                Italy
-# 1	                Region	                Friuli
-# 1	                Winemaker	            Saša Radikon
-
-# class Wine: properties, and priorities..
-
-# Priority	Property	            Example Input
-# 1	        Name	                Vino Bianco IGT, Slatnick 
-# 1	        Producer Name           Radikon [ Import from Producer Class.. ]
-# 1	        Stock                   ?  [ Unit, Bottles ]
-# 1	        Buying Cost	            ?.??  [ Ex or Inc Vat ]
-# 1	        Selling Cost	        ?.??  [ Inc Vat ]
-# 1	        Country                 Italy  [ Import from Producer Class.. ]
-# 2	        Region                  Friuli [ Import from Producer Class.. ]
-# 2	        Vintage	                2019
-# 2	        Colour	                Orange
-# 2	        Dominant Grape	        80% Chardonnay, 20% Tocai
-# 2	        ABV	                    14%
-# 3	        Type	                Still, Skin Contact
-# 3	        Bottle Size	            75cl
-# 3	        Wine Style	            Full-bodied
-
-
-wine1 = Wine("Vino Bianco IGT, Slatnick", producer1, 12, 16.00, 32.00)
+wine1 = Wine("Vino Bianco IGT, Slatnick", producer1, 24, 15.00, 30.00)
 wine_repository.save(wine1)
 
-wine2 = Wine("Wine Name 2", producer1, 6, 5.00, 10.00)
+wine2 = Wine("Oslavje", producer1, 12, 18.00, 36.00)
 wine_repository.save(wine2)
 
-wine3 = Wine("Wine Name 3", producer1, 6, 5.00, 10.00)
+wine3 = Wine("Jakot", producer1, 4, 19.00, 38.00)
 wine_repository.save(wine3)
 
-wine4 = Wine("Wine Name 4", producer2, 6, 5.00, 10.00)
+wine4 = Wine("Kisi", producer2, 18, 9.00, 18.00)
 wine_repository.save(wine4)
 
-wine5 = Wine("Wine Name 5", producer2, 6, 5.00, 10.00)
+wine5 = Wine("Rkatsiteli", producer2, 12, 10.00, 20.00)
 wine_repository.save(wine5)
 
-wine6 = Wine("Wine Name 6", producer2, 6, 5.00, 10.00)
+wine6 = Wine("Mtsvane", producer2, 4, 12.00, 24.00)
 wine_repository.save(wine6)
 
-wine7 = Wine("Wine Name 7", producer3, 6, 5.00, 10.00)
+wine7 = Wine("Sauvignon, Vom Opok", producer3, 24, 12.00, 24.00)
 wine_repository.save(wine7)
 
-wine8 = Wine("Wine Name 8", producer3, 6, 5.00, 10.00)
+wine8 = Wine("Gräfin", producer3, 12, 14.00, 28.00)
 wine_repository.save(wine8)
 
-wine9 = Wine("Wine Name 9", producer3, 6, 5.00, 10.00)
+wine9 = Wine("Erde", producer3, 0, 18.00, 36.00)
 wine_repository.save(wine9)
 
 wine_repository.select_all()
